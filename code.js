b48428731be10eacec8d57029e7f6bfb1781fb64
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2022-06-13 14:54:20 -0500

    first problem done

--- code.js
+++ code.js
@@ -3,4 +3,16 @@
 // function expression syntax (assigning an anonymous function to a variable)
 const helloWorld = function() {
     return "Hello, World!";
-} hy
+}
+
+helloWorld();
+
+//example that works
+function foo(){
+    return 2
+}
+
+function sayHello(){
+
+}
+
