2b0e51e006b82aca0fc2396094892d2b215e6b7d
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2022-06-13 12:06:42 -0500

    first commit

---
+++ code.js
@@ -0,0 +1,6 @@
+// helloWorld function 
+// function statement syntax
+// function expression syntax (assigning an anonymous function to a variable)
+const helloWorld = function() {
+    return "Hello, World!";
+}
